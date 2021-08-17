ed07b2689b410d340bd02b7af01037218835ecc6
ee-wk <872464959@qq.com>
2021-08-17 19:30:47 +0800

    123

--- runner.py
+++ runner.py
@@ -7,7 +7,7 @@ import pytest
 now = time.strftime("%Y%m%d%H%M%S", time.localtime())  # 获取实时时间
 file_name = f'report_{now}'
 
-pytest.main(['-s', 'test_case/test_login.py', '--alluredir', './temp'])
+pytest.main(['-vs', 'test_case/test_login.py', '--alluredir', './temp'])
 os.system(f'allure generate ./temp -o ./report/{file_name} --clean')
 f = os.getcwd()
 path = f + '\\temp'
